99a38a845295bbc3a7c215100588285d04a3762f
Jason Carter <jason@jason-carter.net>
2018-01-24 21:50:47 -0500

    Playing with Startup Script

--- concord_server/run.sh
+++ concord_server/run.sh
@@ -2,4 +2,9 @@
 set -e
 
 PORT=$(jq --raw-output ".port" $CONFIG_PATH)
+DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
+
+echo "Using $PORT"
+echo "DEBUG: $DEBUG"
+
 /usr/bin/concord232_server --serial $PORT
