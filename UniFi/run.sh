be854d44328148d35ebd7490efe01c93c3bc198b
Jason Carter <jason@jason-carter.net>
2018-01-26 23:28:05 -0500

    Another Try

--- UniFi/run.sh
+++ UniFi/run.sh
@@ -5,68 +5,62 @@ CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 
 
-
-### Update the Certs incase they've changed
-### Ref:  https://gist.github.com/bsodmike/bc56730fcc71e69bf10875898a92de02
 TEMPFILE=$(mktemp)
 CERTTEMPFILE=$(mktemp)
+CERTDIR="/data"
 
-	# Identrust cross-signed CA cert needed by the java keystore for import.
-	# Can get original here: https://www.identrust.com/certificates/trustid/root-download-x3.html
-	cat > ${CERTTEMPFILE} <<'_EOF'
------BEGIN CERTIFICATE-----
-MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/
-MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
-DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow
-PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD
-Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O
-rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq
-OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b
-xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw
-7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD
-aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV
-HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG
-SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69
-ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr
-AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz
-R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5
-JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo
-Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ
------END CERTIFICATE-----
-_EOF
+# SSL certificate setup
+# Ref:  https://github.com/goofball222/unifi/blob/master/stable/root/usr/local/bin/docker-entrypoint.sh
+if [ -e /ssl/${KEYFILE} ] && [ -e ${CERTDIR}/fullchain.pem ]; then
+    if `/usr/bin/sha256sum -c ${CERTDIR}/unificert.sha256 &> /dev/null`; then
+        log "INFO - SSL: certificate files unchanged, continuing with UniFi startup"
+        log "INFO - SSL: To force rerun import process: delete '${CERTDIR}/unificert.sha256' and restart the container"
+    else
+        if [ ! -e ${DATADIR}/keystore ]; then
+            log "WARN - SSL: keystore does not exist, generating it with Java keytool"
+            keytool -genkey -keyalg RSA -alias unifi -keystore ${DATADIR}/keystore \
+            -storepass aircontrolenterprise -keypass aircontrolenterprise -validity 1825 \
+            -keysize 4096 -dname "cn=UniFi"
+        else
+            log "INFO - SSL: backup existing '${DATADIR}/keystore' to '${DATADIR}/keystore-$(date +%s)'"
+            cp ${DATADIR}/keystore ${DATADIR}/keystore-$(date +%s)
+        fi
+        log "INFO - SSL: custom certificate keystore update"
+        log "INFO - SSL: openssl combine custom private key and certificate chain into temporary PKCS12 file"
+        openssl pkcs12 -export \
+            -inkey /ssl/${KEYFILE} \
+            -in /ssl/{$CERTFILE} \
+            -out ${TEMPFILE} \
+            -name ubnt -password pass:temppass
 
-echo "Using openssl to prepare certificate..."
-openssl pkcs12 -export  -passout pass:aircontrolenterprise \
-	-in //ssl/${CERTFILE} \
-	-inkey /ssl/${KEYFILE} \
-	-out ${TEMPFILE} -name unifi \
-	-CAfile /ssl/${CERTFILE} -caname root
+        log "INFO - SSL: Java keytool import PKCS12 '${TEMPFILE}' file into '${DATADIR}/keystore'"
+        keytool -importkeystore -deststorepass aircontrolenterprise \
+         -destkeypass aircontrolenterprise -destkeystore ${DATADIR}/keystore \
+         -srckeystore ${TEMPFILE} -srcstoretype PKCS12 \
+         -srcstorepass temppass -alias ubnt -noprompt
 
-echo "Removing existing certificate from Unifi protected keystore..."
-keytool -delete -alias unifi -keystore /data/keystore \
-	-deststorepass aircontrolenterprise
-echo "Inserting certificate into Unifi keystore..."
-keytool -trustcacerts -importkeystore \
-	-deststorepass aircontrolenterprise \
-	-destkeypass aircontrolenterprise \
-	-destkeystore /data/keystore \
-	-srckeystore ${TEMPFILE} -srcstoretype PKCS12 \
-	-srcstorepass aircontrolenterprise \
-	-alias unifi
-rm -f ${TEMPFILE}
-echo "Importing cert into Unifi database..."
-java -jar /usr/lib/unifi/lib/ace.jar import_cert \
-	/ssl/${CERTFILE} \
-	${CERTTEMPFILE}
-    
- rm -f ${CERTTEMPFILE}
+        log "INFO - SSL: Removing temporary PKCS12 file"
+        rm ${TEMPFILE}
 
+        log "INFO - SSL: Store SHA256 hash of private key and certificate file"
+        /usr/bin/sha256sum /ssl/${KEYFILE} > ${CERTDIR}/unificert.sha256
+        /usr/bin/sha256sum /ssl/${CERTFILE} >> ${CERTDIR}/unificert.sha256
+
+        log "INFO - SSL: completed update of custom certificate in '${DATADIR}/keystore'"
+        log "INFO - SSL: Check above ***here*** for errors if your custom certificate import isn't working"
+        log "INFO - SSL: To force rerun import process: delete '${CERTDIR}/unificert.sha256' and restart the container"
+    fi
+else
+    [ -f /ssl/${KEYFILE} ] || log "WARN - SSL: missing '/ssl/${KEYFILE}', cannot update certificate in '${DATADIR}/keystore'"
+    [ -f /ssl/${CERTFILE} ] || log "WARN - SSL: missing '/ssl/${CERTFILE}', cannot update certificate in '${DATADIR}/keystore'"
+    log "WARN - SSL: certificate import was NOT performed"
+fi
+# End SSL certificate setup    
 
 ### This is a simple watcher that checks that the Java process
 ### is running, if not, it will kill this script which is 
 ### streaming the server log to HASSIO console  
-./watch $$ &
+/run/watch $$ &
 
 
 ### Start the Java Process in the Background
